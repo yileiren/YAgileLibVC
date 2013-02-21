7832883fe3bcd9378f3107ef1d62858ecb045f8d
yileiren <yileiren@126.coom>
2013-02-21 13:13:22 +0800

    增加UTF-8数据流测试程序。

--- YNetWorkTestServer/main.cpp
+++ YNetWorkTestServer/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 #include "../include/YNetwork/YConnection.h"
+#include "../include/YDataType/YTextEncode.h"
 
 /*!
  * \brief
@@ -81,6 +83,90 @@ void recvData()
     WSACleanup();
 }
 
+/*!
+ * \brief
+ * 接收Utf8文本。
+ */
+void recvUtf8Text()
+{
+	int acceptPort = 0;
+	std::cout<<"请输入监听端口号:";
+	std::cin>>acceptPort;
+
+	//初始化套接字
+	WORD wVersionRequested = MAKEWORD(1,1);
+	WSADATA wsaData;
+	int err = WSAStartup(wVersionRequested,&wsaData);
+	if ( err != 0 ) 
+	{
+		WSACleanup();
+		std::cout<<"初始化套接字失败！"<<std::endl;
+		return;
+	}
+
+	//创建套接字
+	SOCKET serverSocket = socket(AF_INET,SOCK_STREAM,0);
+	if(INVALID_SOCKET == serverSocket)
+	{
+		WSACleanup();
+		std::cout<<"创建套接字失败！"<<std::endl;
+		return;
+	}
+
+	//绑定端口
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr=htonl(INADDR_ANY);
+	addrSrv.sin_family=AF_INET;
+	addrSrv.sin_port=htons(acceptPort);
+
+	err = bind(serverSocket,(SOCKADDR*)&addrSrv,sizeof(SOCKADDR));
+	if(SOCKET_ERROR == err)
+	{
+		WSACleanup();
+		std::cout<<"绑定端口失败！"<<std::endl;
+		return;
+	}
+
+	//设置连接数
+	err = listen(serverSocket,200);
+	if(SOCKET_ERROR == err)
+	{
+		WSACleanup();
+		std::cout<<"初设置连接数失败！"<<std::endl;
+		return;
+	}
+
+	SOCKADDR_IN addrClient;//和客户端连接的发送套接字
+	int len=sizeof(SOCKADDR);
+
+	//监听启动
+	std::cout<<"网络监听已开启！监听端口号："<<acceptPort<<"，等待数据传输..."<<std::endl;
+	SOCKET sockConn=accept(serverSocket,(SOCKADDR*)&addrClient,&len);
+
+	YLR::YDataType::YByteType data;
+	bool r = YLR::YNetWork::YConnection::recaiveData(sockConn,data,50,1000,1000);
+	if(r)
+	{
+		YBYTE * text = new YBYTE[data.getSize()];
+		data.getData(text,data.getSize());
+
+		std::string utf8Str((char *)text);
+
+		std::string * gbkStr = YLR::YDataType::YTextEncode::utf8ToMultibyte(utf8Str);
+
+		std::cout<<*gbkStr<<std::endl;
+		delete[] text;
+		YLR::YDataType::YTextEncode::freeText(gbkStr);
+	}
+	else
+	{
+		std::cout<<"接收数据失败！"<<std::endl;
+	}
+
+	closesocket(serverSocket);//关闭连接
+    WSACleanup();
+}
+
 int main()
 {
 	YLR::YDataType::YByteType b;
@@ -89,6 +175,7 @@ int main()
 		int inputNum = 0; //输入的选择序号。
 		std::cout<<"YNetwork测试程序服务器端已启动，请选择您要进行的操作："<<std::endl;
 		std::cout<<"1：数据接收。"<<std::endl;
+		std::cout<<"2：数据接收（Utf-8格式）。"<<std::endl;
 		std::cout<<"0：退出程序。"<<std::endl;
 		std::cout<<"请输入您的选择：";
 		std::cin>>inputNum;
@@ -101,6 +188,10 @@ int main()
 		else if(1 == inputNum)
 		{
 			recvData();
+		}
+		else if(2 == inputNum)
+		{
+			recvUtf8Text();
 		}
 		else
 		{
